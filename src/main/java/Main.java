ed0f583b18aed4e4d2bda959f9a7a99dd870c4ca
John Lee <ljonghan@gmail.com>
2022-07-27 12:39:07 -0400

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    public static Callable<Integer> getCallableInteger() {
-        // your code here
+    public static Callable<Integer> getCallableInteger(Scanner sc) {
+        return (new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return sc.nextInt();
+            }
+        });
     }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        int val = executor.submit(getCallableInteger(new Scanner(System.in))).get();
+        executor.shutdown();
+    }
+
 }
